c896c29eeb5d9671f0882a678661ecf35ee616e9
yixinfang <fangyix@northeastern.edu>
2023-03-01 21:30:33 -0500

    a6 react sp23

--- src/tuiter/home/single-post-item.js
+++ src/tuiter/home/single-post-item.js
@@ -18,7 +18,7 @@ const PostItem = ({fullPost}) => {
 
                 </div>
                 <div className="col-1">
-                    <div className="float-end"><a><i className="bi bi-three-dots" style={{color:"gray"}}></i></a></div>
+                    <div className="float-end"><a href="/#"><i className="bi bi-three-dots" style={{color:"gray"}}></i></a></div>
                 </div>
 
             </div>
