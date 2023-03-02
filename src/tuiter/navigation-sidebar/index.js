6e2c698ba79cfcc31ca276345c6c0097ef963ab6
yixinfang <fangyix@northeastern.edu>
2023-03-02 11:54:50 -0500

    a6 react sp23

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -9,46 +9,60 @@ const NavigationSidebar = () => {
 
     return (
         <div className="list-group">
-            <a className="list-group-item" href={"/"}>Tuiter</a>
+
+            <Link to={"/"} className={`list-group-item`}>
+                <div className="d-none d-xl-block"><i className="bi bi-twitter wd-icon-width  mt-1"></i> Tuiter</div>
+                <div className={"d-xl-none"}><i className="bi bi-twitter wd-icon-width  mt-1"></i></div>
+            </Link>
 
             <Link to="/tuiter/home" className={`list-group-item ${active === 'home'?'active':''}`}>
-                Home
-            </Link>
-            <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore'?'active':''}`}>
-                Explore
-            </Link>
-            <Link to="/" className="list-group-item">
-                Labs
+                <div className="d-none d-xl-block"><i className="bi bi-house-fill wd-icon-width  mt-1"></i> Home</div>
+                <div className={"d-xl-none"}><i className="bi bi-house-fill wd-icon-width  mt-1"></i></div>
             </Link>
 
+            <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore'?'active':''}`}>
+                <div className="d-none d-xl-block"><i className="bi bi-hash wd-icon-width  mt-1"></i> Explore</div>
+                <div className={"d-xl-none"}><i className="bi bi-hash wd-icon-width  mt-1"></i></div>
+            </Link>
+
+            <Link to={"/"} className="list-group-item">
+                <div className="d-none d-xl-block"><i className="bi bi-tags-fill wd-icon-width  mt-1"></i> Labs</div>
+                <div className={"d-xl-none"}><i className="bi bi-tags-fill wd-icon-width  mt-1"></i></div>
+            </Link>
 
             <Link to={"/"} className={`list-group-item ${active === "Notifications".toLowerCase() ? "active" : ""}`} >
-                <i className="bi bi-bell-fill wd-icon-width  mt-1"></i> Notifications
+                <div className="d-none d-xl-block"><i className="bi bi-bell-fill wd-icon-width  mt-1"></i> Notifications</div>
+                <div className={"d-xl-none"}><i className="bi bi-bell-fill wd-icon-width  mt-1"></i></div>
             </Link>
 
             <Link to={"/"} className={`list-group-item ${active === "Messages".toLowerCase() ? "active" : ""}`} >
-                <i className="bi bi-envelope-fill wd-icon-width  mt-1"></i> Messages
+                <div className="d-none d-xl-block"><i className="bi bi-envelope-fill wd-icon-width  mt-1"></i> Messages</div>
+                <div className={"d-xl-none"}><i className="bi bi-envelope-fill wd-icon-width  mt-1"></i></div>
             </Link>
 
-            <a className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`} href={"/"}>
-                Bookmarks
-            </a>
-            <a className={`list-group-item
-                    ${active === 'lists'?'active':''}`} href={"/"}>
-                Lists
-            </a>
-            <a className={`list-group-item
-                    ${active === 'profile'?'active':''}`} href={"/"}>
-                Profile
-            </a>
+            <Link to={"/"} className={`list-group-item ${active === "Bookmarks".toLowerCase() ? "active" : ""}`} >
+                <div className="d-none d-xl-block"><i className="bi bi-bookmark-fill wd-icon-width  mt-1"></i> Bookmarks</div>
+                <div className={"d-xl-none"}><i className="bi bi-bookmark-fill wd-icon-width  mt-1"></i></div>
+            </Link>
+
+            <Link to={"/"} className={`list-group-item ${active === "Lists".toLowerCase() ? "active" : ""}`} >
+                <div className="d-none d-xl-block"><i className="bi bi-list-ul wd-icon-width  mt-1"></i> Lists</div>
+                <div className={"d-xl-none"}><i className="bi bi-list-ul wd-icon-width  mt-1"></i></div>
+            </Link>
+
+            <Link to={"/"} className={`list-group-item ${active === "Profile".toLowerCase() ? "active" : ""}`} >
+                <div className="d-none d-xl-block"><i className="bi bi-person-fill wd-icon-width  mt-1"></i> Profile</div>
+                <div className={"d-xl-none"}><i className="bi bi-person-fill wd-icon-width  mt-1"></i></div>
+            </Link>
+
             {/*<a className={`list-group-item*/}
             {/*        ${active === 'more'?'active':''}`} href={"/"}>*/}
             {/*    More*/}
             {/*</a>*/}
             <Link to={"/"} className={`list-group-item ${active === "More".toLowerCase() ? "active" : ""}`} >
 
-                <i className="bi bi-filter-circle-fill wd-icon-width  mt-1"></i> More
+                <div className="d-none d-xl-block"><i className="bi bi-filter-circle-fill wd-icon-width  mt-1"></i> More</div>
+                <div className={"d-xl-none"}><i className="bi bi-filter-circle-fill wd-icon-width  mt-1"></i></div>
 
             </Link>
         </div>
