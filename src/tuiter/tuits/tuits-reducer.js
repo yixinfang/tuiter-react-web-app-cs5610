b823de1c6c16d2e86448cec9af40b0d2d4204299
yixinfang <fangyix@northeastern.edu>
2023-04-06 21:40:45 -0400

    a9 Mongo sp23

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -7,23 +7,6 @@ const initialState = {
     loading: false
 }
 
-
-// const currentUser = {
-//     "userName": "NASA",
-//     "handle": "@nasa",
-//     "image": "nasa.png",
-// };
-//
-// const templateTuit = {
-//     ...currentUser,
-//     "topic": "Space",
-//     "time": "2h",
-//     "liked": false,
-//     "replies": 0,
-//     "retuits": 0,
-//     "likes": 0,
-// }
-
 const tuitsSlice = createSlice({
     name: 'tuits',
     initialState,
