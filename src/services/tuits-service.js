cf6cde12f8d2e7dcafad115a89c178571ff38f73
yixinfang <fangyix@northeastern.edu>
2023-04-08 06:39:08 -0400

    a9 mongobd sp23

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
 // const TUITS_API = 'https://tuiter-node-server-app-qcrx.onrender.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
-// console.log(API_BASE)
+
+const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api' || 'https://tuiter-node-server-app-mongod.onrender.com/api/tuits';
+console.log(API_BASE)
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
